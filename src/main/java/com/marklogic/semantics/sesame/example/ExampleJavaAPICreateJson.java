145f6270c8d0997e50ee44f85b88b2eaf803acf9
xquery <jim@webcomposite.com>
2015-06-24 20:39:39 +0200

    added microbenchmarking

--- src/main/java/com/marklogic/semantics/sesame/example/ExampleJavaAPICreateJson.java
+++ src/main/java/com/marklogic/semantics/sesame/example/ExampleJavaAPICreateJson.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.marklogic.semantic.sesame.example;
+package com.marklogic.semantics.sesame.example;
 
 import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.DatabaseClientFactory;
