fe6d5b111841cc22d683add048f84628f5de843a
ssambasu <ssambasu@marklogic.com>
2015-07-29 15:10:34 -0700

    Initial tests

--- marklogic-sesame-functionaltests/src/test/java/com/marklogic/sesame/functionaltests/MarkLogicRepositoryTest.java
+++ marklogic-sesame-functionaltests/src/test/java/com/marklogic/sesame/functionaltests/MarkLogicRepositoryTest.java
@@ -1,19 +1,3 @@
-/* 
- * Licensed to Aduna under one or more contributor license agreements.  
- * See the NOTICE.txt file distributed with this work for additional 
- * information regarding copyright ownership. 
- *
- * Aduna licenses this file to you under the terms of the Aduna BSD 
- * License (the "License"); you may not use this file except in compliance 
- * with the License. See the LICENSE.txt file distributed with this work 
- * for the full License.
- *
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
- * implied. See the License for the specific language governing permissions
- * and limitations under the License.
- */
 package com.marklogic.sesame.functionaltests;
 
 import static org.junit.Assert.assertTrue;
@@ -34,6 +18,9 @@ import org.openrdf.repository.RepositoryConnection;
 /**
  * Integration test suite for implementations of Repository.
  * 
- * @author Jeen Broekstra
+ * @author Srinath S
  */
-public class MarkLogicRepositoryTest {}
+public class MarkLogicRepositoryTest {
+	
+	
+}
