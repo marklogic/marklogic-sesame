cec75a3aedbcdd2a2d020332a790366fbf95767f
ssambasu <ssambasu@marklogic.com>
2015-07-30 11:09:16 -0700

    Adding tests

--- marklogic-sesame-functionaltests/src/test/java/com/marklogic/sesame/functionaltests/MarkLogicRepositoryConnectionTest.java
+++ marklogic-sesame-functionaltests/src/test/java/com/marklogic/sesame/functionaltests/MarkLogicRepositoryConnectionTest.java
@@ -1,6 +1,8 @@
 package com.marklogic.sesame.functionaltests;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -10,6 +12,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openrdf.model.URI;
 import org.openrdf.model.ValueFactory;
+import org.openrdf.query.QueryLanguage;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
@@ -28,7 +31,7 @@ import com.marklogic.sesame.functionaltests.util.ConnectedRESTQA;
 
 public class MarkLogicRepositoryConnectionTest  extends  ConnectedRESTQA{
 
-	private static final String TEST_DIR_PREFIX = "/testdata";
+	private static final String TEST_DIR_PREFIX = "/testdata/";
 	private static String dbName = "MLSesame";
 	private static String [] fNames = {"MLSesame-1"};
 	private static String restServer = "REST-MLSesame-API-Server";
@@ -50,14 +53,14 @@ public class MarkLogicRepositoryConnectionTest  extends  ConnectedRESTQA{
 	@BeforeClass
 	public static void initialSetup() throws Exception {
 		
-		setupJavaRESTServer(dbName, fNames[0], restServer, restPort);
+	/*	setupJavaRESTServer(dbName, fNames[0], restServer, restPort);
 		setupAppServicesConstraint(dbName);
 		enableCollectionLexicon(dbName);
 		enableTripleIndex(dbName);
 		
 		createUserRolesWithPrevilages("test-eval", "xdbc:eval", "xdbc:eval-in", "xdmp:eval-in", "any-uri", "xdbc:invoke");
 		createRESTUser("reader", "reader", "test-eval", "rest-reader");
-		createRESTUser("writer", "writer", "test-eval", "rest-writer");
+		createRESTUser("writer", "writer", "test-eval", "rest-writer");*/
 	}
 	
 /*	@AfterClass
@@ -83,7 +86,8 @@ public class MarkLogicRepositoryConnectionTest  extends  ConnectedRESTQA{
 	{
 		logger.debug("tearing down...");
 		
-		testAdminCon.clear();
+	//	testAdminCon.clear(graph1);
+	//	testAdminCon.clear(graph2);
 		testAdminCon.close();
 		testAdminCon = null;
 
@@ -176,16 +180,68 @@ public class MarkLogicRepositoryConnectionTest  extends  ConnectedRESTQA{
 		
 	@Test
 	public void testPrepareBooleanQuery1() throws Exception{
-		// add file default-graph.ttl to repository, no context
 		InputStream in = MarkLogicRepositoryConnectionTest.class.getResourceAsStream(TEST_DIR_PREFIX
-				+ "directory.ttl");
+				+ "tigers.ttl");
 		testAdminCon.add(in, "", RDFFormat.TURTLE, graph1);
 		in.close();
-		Assert.assertEquals(20L, testAdminCon.size());
+		Assert.assertEquals(107L, testAdminCon.size());
 		
+		String query1 = "PREFIX  bb: <http://marklogic.com/baseball/players#>"+
+						"ASK FROM <http://marklogic.com/Graph1>"+
+						"WHERE"+ 
+						"{"+
+						 "?id bb:lastname  ?name ."+
+						    "FILTER  EXISTS { ?id bb:country ?countryname }"+
+						"}";
 		
+		boolean result1 = testAdminCon.prepareBooleanQuery(QueryLanguage.SPARQL, query1).evaluate();
+		Assert.assertFalse(result1);	
+		
+		String query2 = "PREFIX  bb: <http://marklogic.com/baseball/players#>"+
+				"PREFIX  r: <http://marklogic.com/baseball/rules#>"+
+				"ASK  FROM <http://marklogic.com/Graph1> WHERE"+ 
+				"{"+
+				 "?id bb:team r:Tigers."+
+				    "?id bb:position \"pitcher\"."+
+				"}";
 
+		boolean result2 = testAdminCon.prepareBooleanQuery(QueryLanguage.SPARQL, query2).evaluate();
+		Assert.assertTrue(result2);	
+	}
 	
+	@Test
+	public void testPrepareBooleanQuery2() throws Exception{
+		
+		InputStream in = MarkLogicRepositoryConnectionTest.class.getResourceAsStream(TEST_DIR_PREFIX
+				+ "tigers.ttl");		
+		Reader reader = new InputStreamReader(in);
+		testAdminCon.add(reader, "", RDFFormat.TURTLE, graph1);
+		
+		
+		Assert.assertEquals(107L, testAdminCon.size());
+		reader.close();
+		
+		String query1 = "PREFIX  bb: <http://marklogic.com/baseball/players#>"+
+				"ASK FROM <http://marklogic.com/Graph1>"+
+				"WHERE"+ 
+				"{"+
+				 "<players#119> <players#team> <rules#Tigers>."+
+				 "}";
+
+		boolean result1 = testAdminCon.prepareBooleanQuery(QueryLanguage.SPARQL, query1,"http://marklogic.com/baseball/").evaluate();
+		Assert.assertTrue(result1);	
+		
+		String query2 = "PREFIX  bb: <http://marklogic.com/baseball/players#>"+
+				"PREFIX  r: <http://marklogic.com/baseball/rules#>"+
+				"ASK  FROM <http://marklogic.com/Graph1> WHERE"+ 
+				"{"+
+				 "?id bb:team r:Tigers."+
+				    "?id bb:position \"pitcher\"."+
+				"}";
+
+		boolean result2 = testAdminCon.prepareBooleanQuery(QueryLanguage.SPARQL, query2, "").evaluate();
+		Assert.assertTrue(result2);	
 		
 	}
+
 }
