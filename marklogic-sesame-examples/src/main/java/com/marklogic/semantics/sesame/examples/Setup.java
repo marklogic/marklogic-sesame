e2ab0a0c722c51d6c1b52fc87645518747e10217
xquery <jim@webcomposite.com>
2015-09-09 11:06:57 +0200

    tidy up - added some basic examples

---
+++ marklogic-sesame-examples/src/main/java/com/marklogic/semantics/sesame/examples/Setup.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.marklogic.semantics.sesame.examples;
+
+import com.marklogic.semantics.sesame.MarkLogicRepository;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Setup {
+
+    public static MarkLogicRepository loadPropsAndInit() {
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream("gradle.properties"));
+        } catch (IOException e) {
+            System.err.println("problem loading properties file.");
+            System.exit(1);
+        }
+        String host = props.getProperty("mlHost");
+        int port = Integer.parseInt(props.getProperty("mlRestPort"));
+        String user = props.getProperty("writerUser");
+        String pass = props.getProperty("writerPassword");
+
+        return new MarkLogicRepository(host,port,user,pass,"DIGEST");
+    }
+}
